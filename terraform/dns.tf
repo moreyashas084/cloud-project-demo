eb612d807646e647a888002c76fe161a374351fb
Yashas More <moreyashas084@gmail.com>
2025-08-02 22:25:34 +0530

    cacs sc asc

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,11 +1,11 @@
 resource "google_dns_managed_zone" "durva_zone" {
   name     = "durva-zone"
-  dns_name = "durva.gcp.com."
+  dns_name = "practice.projects.com."
   description = "Managed zone for durva.dev.com"
 }
 
 resource "google_dns_record_set" "durva_www" {
-  name         = "www.durva.gcp.com."
+  name         = "www.practice.projects.com."
   type         = "A"
   ttl          = 300
   managed_zone = google_dns_managed_zone.durva_zone.name
