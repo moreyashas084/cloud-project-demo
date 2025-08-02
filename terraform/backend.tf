a5652ec7ebb4a59c50c7befde957dcdd13964610
Yashas More <moreyashas084@gmail.com>
2025-08-02 14:03:46 +0530

    bucket reions

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket = "gcpproject-terraform-bucket"
+    bucket  = "gcpproject-terraform-bucket"
+    project = "gcppracticedemo-467805"
+    region  = "us-central1"
   }
 }
