a948bf102a19a880dcb13ac9f9b660e379088a64
Yashas More <moreyashas084@gmail.com>
2026-06-09 08:50:49 +0530

    added configs wif9

--- terraform/secret-manager.tf
+++ terraform/secret-manager.tf
@@ -1,6 +1,6 @@
-module "ssl_certificate" {
-  source = "./modules/ssl-certificate"
-  project_id       = var.gcp_project
-  certificate_name = "my-app-certificate"
-  domains          = ["www.practice.projects.com"]
-}
+# module "ssl_certificate" {
+#   source = "./modules/ssl-certificate"
+#   project_id       = var.gcp_project
+#   certificate_name = "my-app-certificate"
+#   domains          = ["www.practice.projects.com"]
+# }
