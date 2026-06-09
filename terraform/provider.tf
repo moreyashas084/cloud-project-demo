4b33712e43d65a8d0cb4fdf03f9335e3490a8060
Yashas More <moreyashas084@gmail.com>
2026-06-09 09:14:02 +0530

    creating gke cluster

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,14 @@
+terraform {
+    required_version = ">= 1.0.0"
+    required_providers {
+        google = {
+            source  = "hashicorp/google"
+            version = ">= 4.0.0"
+        }
+    }
+}
+
 provider "google" {
     project = var.gcp_project
-    region = var.primary_region
+    region  = var.primary_region
 }
